9427784eecc433f0284662914568b0bf931efe1f
sangboking <cowg001@naver.com>
2024-04-09 16:24:55 +0900

    fix : 로직 전반적으로 수정

--- App.js
+++ App.js
@@ -1,42 +1,9 @@
 import { WebView } from "react-native-webview";
+import { Pressable, Text, View } from "react-native";
+import { useEffect, useRef } from "react";
 import useBLE from "./useBLE";
-import {
-  Button,
-  FlatList,
-  SafeAreaView,
-  ScrollView,
-  Text,
-  TouchableOpacity,
-  View,
-} from "react-native";
-import { useEffect, useRef, useState } from "react";
-
-const ScanDevice = ({ deviceId, name, connectToDevice }) => (
-  <View style={{ backgroundColor: "lightblue", marginTop: 12 }}>
-    <Text style={{ fontSize: 16 }}>{`deviceId : ${deviceId}`}</Text>
-    <Text>{`deviceName : ${name}`}</Text>
-    <Button
-      color="gray"
-      title="해당기기 연결하기"
-      onPress={() => connectToDevice(deviceId)}
-    />
-  </View>
-);
-
-const ConnectedDevice = ({ deviceId, name, disconnectFromDevice }) => (
-  <View style={{ backgroundColor: "lightblue", marginTop: 12 }}>
-    <Text style={{ fontSize: 16 }}>{`deviceId : ${deviceId}`}</Text>
-    <Text>{`deviceName : ${name}`}</Text>
-    <Button
-      color="red"
-      title="해당기기 연결 해제하기"
-      onPress={() => disconnectFromDevice(deviceId)}
-    />
-  </View>
-);
 
 export default function App() {
-  const [toggle, setToggle] = useState(true);
   const {
     scanForPeripherals,
     stopScanForPeripherals,
@@ -46,121 +13,57 @@ export default function App() {
     allDevices,
     connectedDevice,
   } = useBLE();
-  const uri = "https://playful-treacle-e405aa.netlify.app/";
+
+  //랜더링할 webview 주소 기입해 주세요. 로컬테스트시 아래와 같이 ip주소로 열어야 합니다.
+  const uri = "http://192.168.0.125:5173/";
+
   const webViewRef = useRef();
 
-  /* native -> web */
-  const native_to_web = () => {
-    webViewRef.current.postMessage("test");
+  // WebView -> RN : 넘겨받는 type 값에 따라 기기 연결, 해제 로직 수행
+  const receiveMessageToWebView = async (e) => {
+    const data = JSON.parse(e.nativeEvent.data);
+
+    if (data.type === "connect") {
+      connectToDevice(data.id);
+    }
+
+    if (data.type === "disconnect") {
+      disconnectFromDevice(data.id);
+    }
   };
 
-  const sendMessage = () => {
-    const sendData = JSON.stringify({
-      type: "cend",
-      id: 1,
-      name: "testName",
-      content: "test content",
-      file: null,
-    });
+  // 디바이스 스캔시 allDevices에 값이 들어올때마다 RN -> WebView로 데이터 전달
+  useEffect(() => {
+    webViewRef.current.postMessage(JSON.stringify(allDevices));
+  }, [allDevices]);
 
-    webViewRef.current.postMessage(sendData);
-  };
+  // 디바이스 연결, 해제 될때마다 connectedDevice값이 바뀌며 RN -> WebView로 데이터 전달
+  useEffect(() => {
+    webViewRef.current.postMessage(JSON.stringify(connectedDevice));
+  }, [connectedDevice]);
 
   return (
-    <View>
-      <WebView ref={webViewRef} source={{ uri: uri }} />
+    <>
+      <View style={{ flex: 0 }}>
+        <Pressable
+          style={{ marginTop: 30, backgroundColor: "skyblue", padding: 5 }}
+          onPress={() => scanForDevices()}
+        >
+          <Text>BLE 기기 스캔 버튼</Text>
+        </Pressable>
 
-      <TouchableOpacity onPress={sendMessage}>
-        <Text style={{ marginTop: 20 }}>웹뷰로 데이터 전송</Text>
-      </TouchableOpacity>
-    </View>
-
-    // <WebView ref={webViewRef} source={{ uri: uri }} />
-
-    // <ScrollView style={{ paddingBottom: 30 }}>
-    //   <View
-    //     style={{
-    //       flexDirection: "row",
-    //       alignItems: "center",
-    //       justifyContent: "center",
-
-    //       marginTop: 40,
-    //     }}
-    //   >
-    //     <Text
-    //       style={{
-    //         width: 100,
-    //         height: 30,
-    //         backgroundColor: "skyblue",
-    //         textAlign: "center",
-    //         verticalAlign: "middle",
-    //       }}
-    //       onPress={() => setToggle(true)}
-    //     >
-    //       디바이스 스캔
-    //     </Text>
-    //     <Text
-    //       style={{
-    //         width: 100,
-    //         height: 30,
-    //         backgroundColor: "pink",
-    //         textAlign: "center",
-    //         verticalAlign: "middle",
-    //         marginLeft: 20,
-    //       }}
-    //       onPress={() => setToggle(false)}
-    //     >
-    //       연결된 디바이스
-    //     </Text>
-    //   </View>
-
-    //   {toggle ? (
-    //     <>
-    //       <Button
-    //         color="#f194ff"
-    //         title="디바이스 스캔 시작"
-    //         onPress={() => scanForDevices()}
-    //       />
-
-    //       <Button
-    //         title="디바이스 스캔 중지"
-    //         onPress={() => stopScanForPeripherals()}
-    //       />
-
-    //       {allDevices.length >= 1 && (
-    //         <>
-    //           <Text style={{ fontSize: 18, marginTop: 20 }}>
-    //             스캔된 디바이스 리스트
-    //           </Text>
-    //           <SafeAreaView>
-    //             <FlatList
-    //               data={allDevices}
-    //               renderItem={(item) => (
-    //                 <ScanDevice
-    //                   deviceId={item.item.id}
-    //                   name={item.item.name}
-    //                   connectToDevice={connectToDevice}
-    //                 />
-    //               )}
-    //             />
-    //           </SafeAreaView>
-    //         </>
-    //       )}
-    //     </>
-    //   ) : (
-    //     <>
-    //       <Text style={{ fontSize: 18, marginTop: 25 }}>연결된 디바이스</Text>
-    //       <ConnectedDevice
-    //         deviceId={
-    //           connectedDevice?.id ? connectedDevice?.id : "연결된 기기 없음"
-    //         }
-    //         name={
-    //           connectedDevice?.name ? connectedDevice?.name : "연결된 기기 없음"
-    //         }
-    //         disconnectFromDevice={disconnectFromDevice}
-    //       />
-    //     </>
-    //   )}
-    // </ScrollView>
+        <Pressable
+          style={{ marginTop: 10, backgroundColor: "red", padding: 5 }}
+          onPress={() => stopScanForPeripherals()}
+        >
+          <Text>BLE 기기 스캔 중지 버튼</Text>
+        </Pressable>
+      </View>
+      <WebView
+        ref={webViewRef}
+        source={{ uri: uri }}
+        onMessage={receiveMessageToWebView}
+      />
+    </>
   );
 }
